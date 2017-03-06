326e437cd1d7e785d81094148c8629160cda325a
cosmos342 <sbhat2@yahoo.com>
2017-03-06 10:11:07 -0800

    Add files via upload

--- VisionClassifier.py
+++ VisionClassifier.py
@@ -1,29 +1,25 @@
-# prepare data augmentation configuration
+## prepare data augmentation configuration
 import os
 from keras.preprocessing import image
 from keras.utils.np_utils import to_categorical
 
 from matplotlib import pyplot as plt
 import argparse
-import bcolz
 
-import pdb
-pdb.set_trace()
+#import pdb
+#pdb.set_trace()
 mpath =  os.getcwd()
-train_data_dir= mpath + '/data/train'
-validation_data_dir= mpath + '/data/valid'
-test_data_dir= mpath + '/data/test/'
+train_data_dir= mpath + '/data/dogscats/train'
+#train_data_dir= mpath + '/data/dogscats/sample/train'
+validation_data_dir= mpath + '/data/dogscats/valid'
+#validation_data_dir= mpath + '/data/dogscats/sample/valid'
+#test_data_dir= mpath + '/dogscats/test/'
+test_data_dir= mpath + '/data/dogscats/test'
 saved_dir = mpath + '/saved/'
 results_dir = mpath + '/results/'
 
 from VisionModel import *
 
-def save_array(fname, arr):
-    c=bcolz.carray(arr, rootdir=fname, mode='w')
-    c.flush()
-
-def load_array(fname):
-    return bcolz.open(fname)[:]
 
 
 def plots(ims, figsize=(12,6), rows=1, interp=False, titles=None):
@@ -51,12 +47,18 @@ parser.add_argument('--topmodel', type=str,default="load",
                     help='load or fresh trained features')
 parser.add_argument('--batchnorm', type=str,default="yes",
                     help='load or fresh trained features')
-parser.add_argument('--numepoch', type=int,default=3,
+parser.add_argument('--numepoch', type=int,default=1,
                     help='load or fresh trained features')
-parser.add_argument('--lr', type=float,default=0.01,
+parser.add_argument('--lr', type=float,default=0.001,
                     help='load or fresh trained features')
 parser.add_argument('--testonly', type=bool,default=False,
                     help='test only no training')
+parser.add_argument('--model',type=str,default="simple",
+		    help='specify which type of model to finetune')
+parser.add_argument('--regularizer',type=str,default="none",
+                    help='specify l2 or l1 weight regularizer')
+			
+
 
 args = parser.parse_args()
 
@@ -66,43 +68,63 @@ batch_norm = args.batchnorm
 num_epoch = args.numepoch
 learn_rate = args.lr
 test_only = args.testonly
+model_type = args.model
+regularizer = args.regularizer
 
-trainedModel = VisionTrainedModel(train_data_dir,validation_data_dir,test_data_dir,saved_dir)
+print("create trained model")
+
+def createTrainedModel():
+	return VisionTrainedModel(train_data_dir,validation_data_dir,test_data_dir,saved_dir,model_type)
+
+def createAugmentedTrainedModel():
+	return VisionTrainedModel(train_data_dir,validation_data_dir,test_data_dir,saved_dir,model_type,rotation_range=10, width_shift_range=0.05, width_zoom_range=0.05, zoom_range=0.05, channel_shift_range=10, height_shift_range=0.05, shear_range=0.05, horizontal_flip=True)
+
+
+if(model_type == "aug"):
+	trainedModel = createAugmentedTrainedModel()
+else:
+	trainedModel = createTrainedModel()
+
+print("done creating trained model ")
+
 trainedModel.createDataGen()
+train_labels = trainedModel.getTrainLabels()
+val_labels = trainedModel.getValLabels()
+#print("nuking gens")
+#trainedModel.nukeGens()
 
 if(tf_features == "fresh"):
 	print("trainedModel predicting ")
 	trainedModel.predictGenerator()
+	#trainedModel.predict()
 	trainedModel.saveFeatures()
 else:
 	print("trainedModel loading features")
 	trainedModel.loadFeatures()
 
 train_output, val_output, test_output = trainedModel.getFeatures()
-val_gen = trainedModel.getValGen()
-train_labels = trainedModel.getTrainLabels()
-val_labels = trainedModel.getValLabels()
 
-topModel = VisionTopModel()
-
+topModel = VisionTopModel(2,model_type,trainedModel.getFeaturesShape(),regularizer)
 
 if(load_topmodel == "load"):
 	topModel.loadModel(saved_dir , 'dogs1.h5')
 #	topModel.setLearningRate(learn_rate)
 else:
-	topModel.flatten(trainedModel.getFeaturesShape())
+	#topModel.flatten(trainedModel.getFeaturesShape())
+	topModel.fineTune()
 
-	if(batch_norm == "yes"):
-		topModel.addFC(1024)
-	else:
-		topModel.addFC(1024,batch_norm=False)
+	#if(batch_norm == "yes"):
+	#	topModel.addFC(1024)
+	#else:
+	#	topModel.addFC(1024,batch_norm=False)
 
-	topModel.addClassificationLayer(trainedModel.getNumClasses())
+	#topModel.addClassificationLayer(trainedModel.getNumClasses())
 
 topModel.compileModel(lr=learn_rate)
 
 if(test_only == False):
-	history = topModel.fit(train_output, train_labels,val_output,val_labels,nbepoch=num_epoch, batch=32)
+	for i in range(3):
+		history = topModel.fit(train_output, train_labels,val_output,val_labels,nbepoch=num_epoch, batch=100)
 	topModel.saveModel(saved_dir , 'dogs1.h5')
 
 '''
@@ -117,28 +139,28 @@ plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 '''
 
-
+print("predicting topmodel")
 pred = topModel.predict(test_output)
 
 test_gen = trainedModel.getTestGen()
 test_images = test_gen.filenames
 # test_pred = pred[:,0]
-save_array(results_dir + 'test_preds.dat', pred)
-save_array(results_dir + 'filenames.dat', test_images)
+#save_array(results_dir + 'test_preds.dat', pred)
+#save_array(results_dir + 'filenames.dat', test_images)
 #Grab the dog prediction column
 isdog = pred[:,1]
-print "Raw Predictions: " + str(isdog[:5])
-print "Mid Predictions: " + str(isdog[(isdog < .6) & (isdog > .4)])
-print "Edge Predictions: " + str(isdog[(isdog == 1) | (isdog == 0)])
-print "#dog Predictions: " + str(len(isdog[isdog >= 0.5]))
+#print "Raw Predictions: " + str(isdog[:5])
+#print "Mid Predictions: " + str(isdog[(isdog < .6) & (isdog > .4)])
+#print "Edge Predictions: " + str(isdog[(isdog == 1) | (isdog == 0)])
+#print "#dog Predictions: " + str(len(isdog[isdog >= 0.5]))
 
 isdog = isdog.clip(min=0.03, max=0.97)
 ids = np.array([int(f[8:f.find('.')]) for f in test_images])
 
 subm = np.stack([ids,isdog], axis=1)
-subm[:5]
+#subm[:5]
 
-submission_file_name = 'submission1.csv'
+submission_file_name = 'submission' + model_type + '.csv'
 np.savetxt(results_dir + submission_file_name, subm, fmt='%d,%.5f', header='id,label', comments='')
 
 '''
--- VisionModel.py
+++ VisionModel.py
@@ -1,4 +1,6 @@
-from keras.applications.vgg16 import  VGG16
+import vgg16; reload(vgg16)
+from vgg16 import VGG16
+#from keras.applications.vgg16 import  VGG16 
 from keras.preprocessing import image
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
@@ -8,6 +10,7 @@ from keras.layers import Flatten,Dense, BatchNormalization, Activation
 from keras.layers.convolutional import Convolution2D
 from keras.optimizers import Adam
 from keras.regularizers import l2
+import bcolz
 
 MODEL_VGG16 = 1
 MODEL_VGG19 = 2
@@ -15,19 +18,50 @@ MODEL_RESNET = 3
 MODEL_INCEPTION = 4
 MODEL_TOP = 5
 
+#vgg_mean = np.array([123.68, 116.779, 103.939], dtype=np.float32).reshape((3,1,1))
+vgg_mean = np.array([123.68, 116.779, 103.939], dtype=np.float32)
+
+def save_array(fname, arr):
+    c=bcolz.carray(arr, rootdir=fname, mode='w')
+    c.flush()
+
+def load_array(fname):
+    return bcolz.open(fname)[:]
+
+def vgg_preprocess(x,num):
+	print("vgg process start")
+	my_mean = np.tile(vgg_mean,(num,1))
+	my_mean = my_mean.reshape((num,3,1,1))
+	x = x - my_mean 
+	print("vgg process end")
+
+    	return x[:, ::-1] # reverse axis rgb->bgr
+
+def concat_data(data_gen):
+	return np.concatenate([data_gen.next() for i in range(data_gen.nb_sample)])
+	#for i in range(data_gen.nb_sample):
+	#	ndata = data_gen.next()
+
+
 def oneHot(x):
 	return to_categorical(x)
 
 class VisionModel:
-	def __init__(self,train_dir,val_dir,test_dir,saved_dir):
-		self.train_datagen = image.ImageDataGenerator(
-        				rescale=1./255,
-        				shear_range=0.2,
-        				zoom_range=0.2,
-        				horizontal_flip=True)
+	def __init__(self,train_dir,val_dir,test_dir,saved_dir,
+		width_shift_range=0, width_zoom_range=0, zoom_range=0, 
+		channel_shift_range=0, height_shift_range=0, shear_range=0, 
+		horizontal_flip=False):
+		self.train_datagen = image.ImageDataGenerator()
+		#self.train_datagen = image.ImageDataGenerator(
+        	# 			rescale=1./255,
+        	#			shear_range=0.2,
+        	#			zoom_range=0.2,
+        	#			horizontal_flip=True)
+		self.val_datagen = image.ImageDataGenerator()
+		self.test_datagen = image.ImageDataGenerator()
 
-		self.val_datagen = image.ImageDataGenerator(rescale=1./255)
-		self.test_datagen = image.ImageDataGenerator(rescale=1./255)
+		#self.val_datagen = image.ImageDataGenerator(rescale=1./255)
+		#self.test_datagen = image.ImageDataGenerator(rescale=1./255)
 
 		self.train_dir =train_dir
 		self.test_dir  =test_dir
@@ -35,8 +69,9 @@ class VisionModel:
 		self.saved_dir = saved_dir
 	
 
-	def createDataGen(self,height=224,width=224,batch=32,entropy=None):
+	def createDataGen(self,height=224,width=224,batch=1,entropy=None):
 
+		print("createTrainGen train")
 		self.train_gen = self.train_datagen.flow_from_directory(
         					self.train_dir,
         					target_size=(height, width),
@@ -44,9 +79,14 @@ class VisionModel:
 						class_mode=entropy,
 						shuffle=False)
 		self.num_classes = self.train_gen.nb_class
+		#self.train_imgs = concat_data(self.train_gen)
+		#save_array(self.saved_dir+'train_data.bc',self.train_imgs)
+		#self.train_imgs = load_array(self.saved_dir+'train_data.bc')
+		#self.train_imgs = vgg_preprocess(self.train_imgs,self.train_gen.nb_sample)
 
-		print("createTrainGen")
+
         					
+		print("createTrainGen val")
 
 		self.val_gen = self.val_datagen.flow_from_directory(
         						self.val_dir,
@@ -54,7 +94,12 @@ class VisionModel:
         						batch_size=batch,
         						class_mode=entropy,
 							shuffle=False)
-		print("createValGen")
+		#self.val_imgs = concat_data(self.val_gen)
+		#self.val_imgs = load_array(self.saved_dir+'val_data.bc')
+		#save_array(self.saved_dir+'val_data.bc',self.val_imgs)
+		#self.val_imgs = vgg_preprocess(self.val_imgs,self.val_gen.nb_sample)
+
+		print("createTestGen")
 
 		self.test_gen = self.test_datagen.flow_from_directory(
         						self.test_dir,
@@ -62,8 +107,14 @@ class VisionModel:
         						batch_size=batch,
 							class_mode=entropy,
 							shuffle=False)
+#
+		#self.test_imgs = concat_data(self.test_gen)
+		#self.test_imgs = load_array(self.saved_dir+'test_data.bc')
+		#save_array(self.saved_dir+'test_data.bc',self.test_imgs)
+		#print("done createTesGen saving ")
+		#self.test_imgs = vgg_preprocess(self.test_imgs,self.test_gen.nb_sample)
 
-		print("createTestGen")
+		print("createTestGenFinito")
 
 	def getNumClasses(self):
 		return self.num_classes
@@ -73,6 +124,11 @@ class VisionModel:
 
 	def getValLabels(self):
 		return oneHot(self.val_gen.classes)
+
+	def nukeGens(self):
+		self.val_gen = []
+		self.train_gen = []
+		self.test_gen = []
 
 	def getTestGen(self):
 		return self.test_gen
@@ -88,33 +144,57 @@ class VisionModel:
 
 class VisionTrainedModel(VisionModel):
 
-	def __init__(self,train_dir,val_dir,test_dir,saved_dir,type=MODEL_VGG16,keep_top=False):
-		VisionModel.__init__(self,train_dir,val_dir,test_dir,saved_dir)
+	def __init__(self,train_dir,val_dir,test_dir,saved_dir,model_type,
+		type=MODEL_VGG16,rotation_range=0, 
+		width_shift_range=0, width_zoom_range=0, zoom_range=0, 
+		channel_shift_range=0, height_shift_range=0, shear_range=0, 
+		horizontal_flip=False):
+		VisionModel.__init__(self,train_dir,val_dir,test_dir,saved_dir,
+		width_shift_range=0, width_zoom_range=0, zoom_range=0, 
+		channel_shift_range=0, height_shift_range=0, shear_range=0, 
+		horizontal_flip=False)
 		self.type = type
+		self.model_type = model_type
 		if(self.type == MODEL_VGG16):
-			self.model = VGG16(keep_top,weights='imagenet')
+			if(self.model_type == "simple"):
+				self.model = VGG16(include_top=True,weights='imagenet')
+			else:
+				self.model = VGG16(include_top=False,weights='imagenet')
 		else:
 			print("ERROR: only VCG16 model is supported")
 
 	def predictGenerator(self):
-		self.train_X = self.model.predict_generator(self.train_gen, 23000)
-		self.val_X =  self.model.predict_generator(self.val_gen,2000)
+		self.train_X = self.model.predict_generator(self.train_gen, self.train_gen.nb_sample)
+		self.val_X =  self.model.predict_generator(self.val_gen,self.val_gen.nb_sample)
 		# need to know test size
-		self.test_X =  self.model.predict_generator(self.test_gen,12500)
+		self.test_X =  self.model.predict_generator(self.test_gen,self.test_gen.nb_sample)
+
+	def predict(self):
+		print("trainedModel Predict train")
+		self.train_X = self.model.predict(self.train_imgs,batch_size=8)
+		print("trainedModel Predict val")
+		self.val_X = self.model.predict(self.val_imgs,batch_size=8)
+		print("trainedModel Predict test")
+		self.test_X = self.model.predict(self.test_imgs,batch_size=8)
 
 	def saveFeatures(self):
 		if(self.train_X is not None ):
-                	np.save(open(self.saved_dir + '/bottleneck_features_train.npy', 'w'), self.train_X)
+			print("trainedModel save train features")
+                	np.save(open(self.saved_dir + '/bottleneck_features_train' + self.model_type + '.npy', 'w'), self.train_X)
 		if(self.val_X is not None ):
-                	np.save(open(self.saved_dir + '/bottleneck_features_val.npy', 'w'), self.val_X)
+			print("trainedModel save val features")
+                	np.save(open(self.saved_dir + '/bottleneck_features_val'+ self.model_type +'.npy' , 'w'), self.val_X)
 		if(self.test_X is not None ):
-                	np.save(open(self.saved_dir + '/bottleneck_features_test.npy', 'w'), self.test_X)
+			print("trainedModel save test features")
+               		np.save(open(self.saved_dir + '/bottleneck_features_test' + self.model_type + '.npy', 'w'), self.test_X)
+
 
 	def loadFeatures(self):
-		self.train_X = np.load(open(self.saved_dir + '/bottleneck_features_train.npy'))
-		self.val_X = np.load(open(self.saved_dir + '/bottleneck_features_val.npy'))
-		self.test_X = np.load(open(self.saved_dir + '/bottleneck_features_test.npy'))
-
+		self.train_X = np.load(open(self.saved_dir + '/bottleneck_features_train' + self.model_type + '.npy'))
+		self.val_X = np.load(open(self.saved_dir + '/bottleneck_features_val' + self.model_type + '.npy'))
+	
+		self.test_X = np.load(open(self.saved_dir + '/bottleneck_features_test' + self.model_type + '.npy'))
+	
 	def getFeatures(self):
 		return self.train_X, self.val_X, self.test_X
 
@@ -127,11 +207,24 @@ class VisionTrainedModel(VisionModel):
 
 
 class VisionTopModel:
-	def __init__(self):
+	def __init__(self,num_classes,model_type,input_shape,regularizer):
 		self.model = Sequential();
+		self.num_classes = num_classes
+		self.input_shape = input_shape
+		self.model_type = model_type
+		self.regularizer = regularizer
 
-	def flatten(self,shape):
-		self.model.add(Flatten(input_shape=shape))
+	def fineTune(self):
+		if(self.model_type == "simple"):
+			self.model = Sequential([ Dense(self.num_classes, activation='softmax', input_shape=self.input_shape) ])
+		else:
+			self.flatten()
+			self.addFC(2,4096, batch_norm=True)
+			self.addClassificationLayer()
+		print("TopModel add dense ");
+
+	def flatten(self):
+		self.model.add(Flatten(input_shape=self.input_shape))
 
 	def addConv(self,num_filter,nb_row,nb_col):
 		# self.model.add(Convolution2D(filters, length, width, border='same',activation='relu'))
@@ -139,15 +232,20 @@ class VisionTopModel:
 		self.model.add(BatchNormalization())
 		self.model.add(Activation('relu'))
 
-	def addFC(self,num_neurons, batch_norm=True):
+	def addFC(self,num_layers,num_neurons, batch_norm=True):
 		if(batch_norm == False):
 			print("addFC without batch norm ")
 			self.model.add(Dense(num_neurons,activation='relu', W_regularizer=l2(0.01)))
 		else:
 			print("addFC add batch norm ")
-			self.model.add(Dense(num_neurons))
-			self.model.add(BatchNormalization())
-			self.model.add(Activation('relu'))
+			for i in range(num_layers):
+				if(self.regularizer == "none"):
+					self.model.add(Dense(num_neurons,activation='relu'))
+				elif(self.regularizer == "l2"):
+					self.model.add(Dense(num_neurons,activation='relu',W_regularizer=l2(0.01)))
+				else:
+					self.model.add(Dense(num_neurons,activation='relu',W_regularizer=l1(0.01)))
+				self.model.add(BatchNormalization())
 
 	def disableWeightUpdate(self):
 		for layer in self.model.layers:
@@ -159,21 +257,29 @@ class VisionTopModel:
 	def setLearningRate(self,rate):
 		self.model.optimizer.lr=rate
 		
+	def fineTuneVCG16TopLayer(self,num_out):
+		#self.model.add(Dense(num_out,activation='softmax',input_shape=(1000,)))
+		self.model = Sequential([ Dense(2, activation='softmax', input_shape=(1000,)) ])
+		print("TopModel add dense ");
 
-	def addClassificationLayer(self,num_classes):
-		self.num_classes = num_classes
-		self.model.add(Dense(self.num_classes,activation='softmax'))
+	def addClassificationLayer(self):
+		if(self.regularizer == "none"):
+			self.model.add(Dense(self.num_classes,activation='softmax'))
+		elif(self.regularizer == "l2"):
+			self.model.add(Dense(self.num_classes,activation='softmax',W_regularizer=l2(0.01)))
+		else:
+			self.model.add(Dense(self.num_classes,activation='softmax',W_regularizer=l1(0.01)))
 
     	def compileModel(self, lr=0.01):
 		# to do add binary cross entropy
 		self.model.compile(optimizer=Adam(lr=lr), loss='categorical_crossentropy', metrics=['accuracy'])
 
 	def saveModel(self,mypath,filename):
-		self.model.save(mypath+filename)
+		self.model.save(mypath+ self.model_type + filename)
 
 	def loadModel(self,mypath,filename):
 		print("loading vision model")
-		self.model = load_model(mypath+filename)
+		self.model = load_model(mypath+self.model_type + filename)
 
 	def fit(self,train_input,train_labels,val_input,val_labels,batch=32,nbepoch=1):
 
